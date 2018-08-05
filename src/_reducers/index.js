95e5b7c85a015a732e364b4f7cba3e337f943289
Yonatan Krieger <yonatan.k@walkme.com>
2018-08-05 11:19:29 +0300

    adding groups redux flow

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,15 +1,17 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 
-import { authentication } from './authentication.reducer';
-import { registration } from './registration.reducer';
-import { users } from './users.reducer';
-import { alert } from './alert.reducer';
+import {authentication} from './authentication.reducer';
+import {registration} from './registration.reducer';
+import {users} from './users.reducer';
+import {alert} from './alert.reducer';
+import {groups} from './groups.reducer';
 
 const rootReducer = combineReducers({
-  authentication,
-  registration,
-  users,
-  alert
+    authentication,
+    registration,
+    users,
+    alert,
+    groups
 });
 
 export default rootReducer;
